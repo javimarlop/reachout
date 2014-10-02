38953dcefc9253fd3fde5f3262f1a00a0d28d098
Javier MARTINEZ LOPEZ <javier.martinez@um.es>
2014-10-02 14:42:00 +0200

    last

--- reachout.R
+++ reachout.R
@@ -4,8 +4,8 @@ library(maps)
     reqURL <- "https://api.twitter.com/oauth/request_token"
             accessURL <- "http://api.twitter.com/oauth/access_token"
             authURL <- "http://api.twitter.com/oauth/authorize"
-            consumerKey <- "DrLL7PfIzW66t0sMUPPFvA"
-            consumerSecret <- "IDLFOfvQ9AQlFA2wQ70gTxR1VJufjH2ZntTXXU0L8"
+            consumerKey <- "xxxx"
+            consumerSecret <- "xxx"
             twitCred <- OAuthFactory$new(consumerKey=consumerKey,
                                          consumerSecret=consumerSecret,
                                          requestURL=reqURL,
